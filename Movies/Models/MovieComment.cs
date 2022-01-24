351994baa7fdf81e3905abd4703ffc2c61f029fe
DENISA ILEANA NEACSU <denisa.neacsu@s.unibuc.ro>
2022-01-24 14:17:24 +0200

    fix movie comment

--- Movies/Models/MovieComment.cs
+++ Movies/Models/MovieComment.cs
@@ -8,6 +8,7 @@ namespace Movies.Models
     public class MovieComment
     {
         public int Id { get; set; }
+        public int MovieId { get; set; }
         public Movie Movie { get; set; }
         public string NumePersoana { get; set; }
         public string Comentariu { get; set; }
