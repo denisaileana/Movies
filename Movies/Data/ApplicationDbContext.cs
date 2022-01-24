62ea8024287ff0e397f0720f40cd51d3e3ec3925
DENISA ILEANA NEACSU <denisa.neacsu@s.unibuc.ro>
2022-01-24 16:44:09 +0200

    added users and roles - seed

--- Movies/Data/ApplicationDbContext.cs
+++ Movies/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using IdentityServer4.EntityFramework.Options;
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using Movies.Models;
@@ -31,7 +32,55 @@ namespace Movies.Data
             builder.Entity<MovieGenre>().HasOne(mg => mg.Genre).WithMany(g => g.MovieGenres).HasForeignKey(mg => mg.GenreId);
             builder.Entity<MovieGenre>().HasOne(mg => mg.Movie).WithMany(m => m.MovieGenres).HasForeignKey(mg => mg.MovieId);
 
+
+            builder.Entity<IdentityRole>().HasData(new IdentityRole
+            {
+                Id = "3747e4d8-78dc-4f55-8b38-ab5deea866fb",
+                Name = "Administrator",
+                NormalizedName = "Administrator".ToUpper()
+            }, new IdentityRole
+            {
+                Id = "1ad6ef77-73fe-492f-bb24-ef5827eb7c8c",
+                Name = "User",
+                NormalizedName = "User".ToUpper()
+            });
+
+            var hasher = new PasswordHasher<ApplicationUser>();
+
+            builder.Entity<ApplicationUser>().HasData(new ApplicationUser
+            {
+                Id = "101ab9b6-2b6e-494f-a72a-7ece0c979173",
+                UserName = "admin",
+                NormalizedUserName = "admin".ToUpper(),
+                PasswordHash = hasher.HashPassword(null, "parola")
+            }, new ApplicationUser
+            {
+                Id = "69c272d3-1de9-4ca6-9c4d-61559167aa03",
+                UserName = "user",
+                NormalizedUserName = "user".ToUpper(),
+                PasswordHash = hasher.HashPassword(null, "parola")
+            });
+
+            builder.Entity<IdentityUserRole<string>>().HasData(
+                new IdentityUserRole<string>
+                {
+                    UserId = "101ab9b6-2b6e-494f-a72a-7ece0c979173",
+                    RoleId = "3747e4d8-78dc-4f55-8b38-ab5deea866fb"
+                },
+                new IdentityUserRole<string>
+                {
+                    UserId = "101ab9b6-2b6e-494f-a72a-7ece0c979173",
+                    RoleId = "1ad6ef77-73fe-492f-bb24-ef5827eb7c8c"
+                },
+                new IdentityUserRole<string>
+                {
+                    UserId = "69c272d3-1de9-4ca6-9c4d-61559167aa03",
+                    RoleId = "1ad6ef77-73fe-492f-bb24-ef5827eb7c8c"
+                }
+            );
         }
+
+
 
     }
 }
---
+++ Movies/Data/Migrations/20220124144329_AddUsersAndRoles.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Movies.Data.Migrations
+{
+    public partial class AddUsersAndRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "3747e4d8-78dc-4f55-8b38-ab5deea866fb", "1dcc2486-58ae-496b-a793-0ffd0c3feefb", "Administrator", "ADMINISTRATOR" },
+                    { "1ad6ef77-73fe-492f-bb24-ef5827eb7c8c", "9101be03-d486-43c8-bb4f-ac8ade9aaec7", "User", "USER" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[,]
+                {
+                    { "101ab9b6-2b6e-494f-a72a-7ece0c979173", 0, "9b404c33-01f6-4afd-8c26-59f86b30e8bc", null, false, false, null, null, "ADMIN", "AQAAAAEAACcQAAAAECME8aGLe8gm79tdWDnSVuUHdlpoxUuch1549ZF4NThDeO2iKsiYbEYh9bOvz7WFbQ==", null, false, "5c45ca93-19e1-4d94-a303-e6442ece3c7a", false, "admin" },
+                    { "69c272d3-1de9-4ca6-9c4d-61559167aa03", 0, "8491464b-aeea-4ab4-93f7-76802d616729", null, false, false, null, null, "USER", "AQAAAAEAACcQAAAAEEq0o9fJjpmI8Xpr/pv9EVmpihbnSbG5QTUtCK0gQC2+G1ryeGEBFjabLqb+mPyp9g==", null, false, "78047a94-6e2b-48da-9faf-ba0c7b4ac299", false, "user" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "3747e4d8-78dc-4f55-8b38-ab5deea866fb", "101ab9b6-2b6e-494f-a72a-7ece0c979173" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "1ad6ef77-73fe-492f-bb24-ef5827eb7c8c", "101ab9b6-2b6e-494f-a72a-7ece0c979173" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "1ad6ef77-73fe-492f-bb24-ef5827eb7c8c", "69c272d3-1de9-4ca6-9c4d-61559167aa03" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "1ad6ef77-73fe-492f-bb24-ef5827eb7c8c", "101ab9b6-2b6e-494f-a72a-7ece0c979173" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "3747e4d8-78dc-4f55-8b38-ab5deea866fb", "101ab9b6-2b6e-494f-a72a-7ece0c979173" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "1ad6ef77-73fe-492f-bb24-ef5827eb7c8c", "69c272d3-1de9-4ca6-9c4d-61559167aa03" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1ad6ef77-73fe-492f-bb24-ef5827eb7c8c");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3747e4d8-78dc-4f55-8b38-ab5deea866fb");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "101ab9b6-2b6e-494f-a72a-7ece0c979173");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "69c272d3-1de9-4ca6-9c4d-61559167aa03");
+        }
+    }
+}
